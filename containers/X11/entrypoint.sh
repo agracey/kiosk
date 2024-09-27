862d6a26226d324022bd05cf0f7d655e67ada184
Andrew Gracey <andrew.gracey@suse.com>
2024-09-27 14:37:20 -0700

    Update entrypoint.sh

--- containers/X11/entrypoint.sh
+++ containers/X11/entrypoint.sh
@@ -7,7 +7,24 @@ log() {
 
 log "Starting X11 entrypoint script"
 
-# Check if the DISPLAY environment variable is set
+# Trap signals for graceful shutdown
+trap 'log "Received SIGTERM, initiating shutdown"; cleanup' SIGTERM
+trap 'log "Received SIGINT, initiating shutdown"; cleanup' SIGINT
+
+# Cleanup function to stop all relevant processes
+cleanup() {
+    log "Cleaning up and stopping processes"
+
+    pkill -SIGTERM Xorg
+    pkill -SIGTERM icewm-session-lite
+
+    sleep 3
+
+    log "Cleanup complete, exiting"
+    exit 0
+}
+
+# Set default DISPLAY if not set
 if [ -z "$DISPLAY" ]; then
     log "DISPLAY variable is not set, defaulting to :0"
     DISPLAY=:0
@@ -16,7 +33,7 @@ fi
 # Extract display number from DISPLAY variable
 DISPLAY_NUM=$(echo $DISPLAY | sed 's/^://')
 
-# Clean up the specific X server lock files and sockets
+# Clean up existing X server lock files and sockets
 log "Cleaning up existing X server lock files for display $DISPLAY"
 rm -f /tmp/.X${DISPLAY_NUM}-lock /tmp/.X11-unix/X${DISPLAY_NUM}
 
@@ -24,6 +41,14 @@ if [ $# -gt 0 ]; then
     log "Executing custom command: $@"
     exec "$@"
 else
+    # Start X server
     log "Starting X server on display $DISPLAY"
-    exec startx -- "$DISPLAY"
+    startx -- "$DISPLAY" &
+    X_PID=$!
+
+    # Wait for X server process (Xorg) to finish
+    log "X server (startx) running"
+    wait $X_PID
+
+    log "X server has exited"
 fi
